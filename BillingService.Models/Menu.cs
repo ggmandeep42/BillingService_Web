e056ff7298678b74daac1fc0333c5dfa5b0cf003
ggmandeep42 <60723636+ggmandeep42@users.noreply.github.com>
2021-02-22 16:28:49 -0500

    Add files via upload
    Added BiilingServcie.Models Project

---
+++ BillingService.Models/Menu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BillingService.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public string Price { get; set; }
+        public string IsDrink { get; set; }
+    }
+}
---
+++ BillingService.Models/Order.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BillingService.Models
+{
+    public class Order
+    {
+        public List<KeyValuePair<string, int>> Items { get; set; }
+    }
+}
